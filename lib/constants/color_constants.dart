9f268d54ff6ba54618360ce8e67ebf740e55bd78
Mirkan <mirkancaliskan@gmail.com>
2020-04-18 22:19:41 +0300

    Colors

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+Color backgroundColor = Color(0xFF0E3360);
+
+Color confirmedBoxColor = Color(0xFF4C5CF1);
+
+Color activeBoxColor = Color(0xFFFE7543);
+
+Color recoveredBoxColor = Color(0xFFEFC02C);
+
+Color deceasedBoxColor = Color(0xFFB638F1);
+
+Color liveUpdateBoxColor = Color(0xFFDF565E);
