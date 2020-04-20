529d0e7b670292e11382700d909196c04b4d8200
akcadev <emre@akca.dev>
2020-04-20 13:06:37 +0300

    ui

---
+++ lib/liveupdate.dart
@@ -0,0 +1,211 @@
+import 'package:flutter/material.dart';
+
+import 'constants/color_constants.dart';
+import 'constants/color_constants.dart';
+import 'constants/color_constants.dart';
+import 'constants/color_constants.dart';
+
+class LiveUpdateScreen extends StatefulWidget {
+  LiveUpdateScreen({Key key}) : super(key: key);
+
+  @override
+  _LiveUpdateScreenState createState() => _LiveUpdateScreenState();
+}
+
+class _LiveUpdateScreenState extends State<LiveUpdateScreen> {
+  String country = 'Turkey';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: backgroundColor,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: backgroundColor,
+          elevation: 0.0,
+          title: Text(
+            "Live Update",
+            style: TextStyle(fontWeight: FontWeight.w400, fontSize: 17),
+          ),
+        ),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          margin: EdgeInsets.only(top: 40),
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("liveupdatebg.png"), fit: BoxFit.fill)),
+          child: Stack(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(10),
+                width: 230,
+                height: 130,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Color(0xFF0E3360).withOpacity(0.8)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Select Country",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20,
+                          color: Colors.white),
+                    ),
+                    DropdownButton<String>(
+                      value: country,
+                      style: TextStyle(color: Colors.white),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.white,
+                      ),
+                      isExpanded: true,
+                      onChanged: (String newValue) {
+                        setState(() {
+                          country = newValue;
+                        });
+                      },
+                      items: <String>['Turkey', 'France', 'Italia', 'Germany']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    )
+                  ],
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  padding: EdgeInsets.all(18),
+                  width: double.infinity,
+                  height: 180,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Colors.white,
+                  ),
+                  child: Stack(
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Container(
+                            child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: <Widget>[
+                            Text("742+",
+                                style: TextStyle(
+                                    color: textColor,
+                                    fontWeight: FontWeight.w800,
+                                    fontSize: 19)),
+                            Text("Tested",
+                                style: TextStyle(
+                                    color: textColor,
+                                    fontWeight: FontWeight.w300,
+                                    fontSize: 13))
+                          ],
+                        )),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Image.asset("flag.png"),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(country,
+                                      style: TextStyle(
+                                          color: textColor,
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 18)),
+                                  Text("Istanbul, Beyoglu in Danger",
+                                      style: TextStyle(
+                                          color: textColor,
+                                          fontWeight: FontWeight.w300,
+                                          fontSize: 11))
+                                ],
+                              )
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              VirusInfoBox(
+                                color: Color(0xFFD54141),
+                                imageURL: "arrowtop.png",medicalCount: '626+', medicalTitle: 'Affected',
+                              ),
+                              VirusInfoBox(
+                                color: Color(0xFF58BA50),
+                                imageURL: "arrowbottom.png",medicalCount: '230+', medicalTitle: 'Recoverd',
+                              )
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class VirusInfoBox extends StatelessWidget {
+  const VirusInfoBox(
+      {@required this.color,
+      @required this.medicalCount,
+      @required this.medicalTitle,
+      @required this.imageURL});
+
+  final String medicalCount;
+  final String medicalTitle;
+  final Color color;
+  final String imageURL;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 130,
+      height: 67,
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        color: color,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Image.asset("$imageURL"),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Text("$medicalCount",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w800,
+                      fontSize: 19)),
+              Text("$medicalTitle",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 13))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:covid_tracker/constants/color_constants.dart';
+import 'package:covid_tracker/liveupdate.dart';
 import 'package:flutter/material.dart';
+
+import 'constants/color_constants.dart';
+import 'constants/color_constants.dart';
+import 'constants/color_constants.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        canvasColor: textColor,
         primarySwatch: Colors.blue,
       ),
       home: HomePage(),
@@ -24,60 +30,155 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
+
     return Scaffold(
       backgroundColor: backgroundColor,
-      body: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 100,
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Text(
-                "Stay Home\nStay Safe",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 36,
-                  fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 55,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Stay Home\nStay Safe",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
+              Container(
+                height: 220,
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 5,
+                  crossAxisSpacing: 5,
+                  childAspectRatio: 1.8,
+                  children: [
+                    SummaryBox(
+                      title: "Confirmed",
+                      count: 746,
+                      color: confirmedBoxColor,
+                    ),
+                    SummaryBox(
+                      title: "Active",
+                      count: 626,
+                      color: activeBoxColor,
+                    ),
+                    SummaryBox(
+                      title: "Recovered",
+                      count: 67,
+                      color: recoveredBoxColor,
+                    ),
+                    SummaryBox(
+                      title: "Deceased",
+                      count: 18,
+                      color: deceasedBoxColor,
+                    ),
+                  ],
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LiveUpdateScreen()),
+                  );
+                },
+                child: Ink(
+                  height: 80,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      color: Colors.white,
+                      image: DecorationImage(
+                          image: AssetImage("imagebg.png"), fit: BoxFit.fill)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(13.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Live Update",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 26,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          "about Corona Virus",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w100,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  color: simptomsBoxColor,
+                ),
+                child: Stack(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "5 Simptoms\nabout Corona",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 23,
+                                fontWeight: FontWeight.w600),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 5),
+                            padding: EdgeInsets.only(
+                                top: 13, bottom: 13, right: 40, left: 40),
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(13)),
+                              color: learnMoreButtonColor,
+                            ),
+                            child: Text(
+                              "Learn more",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w300),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Align(
+                        alignment: Alignment.centerRight,
+                        child: Image.asset("doctor.png"))
+                  ],
+                ),
+              )
+            ],
           ),
-          Container(
-            height: 400,
-            child: GridView.count(
-              crossAxisCount: 2,
-              mainAxisSpacing: 5,
-              crossAxisSpacing: 5,
-              childAspectRatio: 2,
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              children: [
-                SummaryBox(
-                  title: "Confirmed",
-                  count: 746,
-                  color: confirmedBoxColor,
-                ),
-                SummaryBox(
-                  title: "Active",
-                  count: 626,
-                  color: activeBoxColor,
-                ),
-                SummaryBox(
-                  title: "Recovered",
-                  count: 67,
-                  color: recoveredBoxColor,
-                ),
-                SummaryBox(
-                  title: "Deceased",
-                  count: 18,
-                  color: deceasedBoxColor,
-                ),
-              ],
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
@@ -96,8 +197,6 @@ class SummaryBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 80,
-      width: 120,
       decoration: BoxDecoration(
         color: color,
         borderRadius: BorderRadius.circular(20.0),
